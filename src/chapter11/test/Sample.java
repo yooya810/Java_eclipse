a728c70251389d6b774dd4c1a02a4806a4cebe2b
yooya810 <goup.peace.321@gmail.com>
2025-07-31 14:49:24 +0900

    numの値変更

--- src/chapter11/test/Sample.java
+++ src/chapter11/test/Sample.java
@@ -5,5 +5,7 @@ class Sample {
 	    Random rand = new Random();
 	    int num = rand.nextInt(10);
 	    System.out.println(num);
+	    num = rand.nextInt(20);
+	    System.out.println(num);
 	  }
 	}
